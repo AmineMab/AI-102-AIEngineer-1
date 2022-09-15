ceb3c57854057203e3114396606cf97f09ddaf77
amineesi <mohamed.boukhaled@gmail.com>
2022-09-15 21:51:29 +0200

    second commit

--- 07-speech/Python/speaking-clock/speaking-clock.py
+++ 07-speech/Python/speaking-clock/speaking-clock.py
@@ -3,23 +3,29 @@ from datetime import datetime
 import os
 
 # Import namespaces
-
+# Import namespaces
+import azure.cognitiveservices.speech as speech_sdk
 
 def main():
     try:
+        
         global speech_config
 
         # Get Configuration Settings
         load_dotenv()
         cog_key = os.getenv('COG_SERVICE_KEY')
+        print(cog_key)
         cog_region = os.getenv('COG_SERVICE_REGION')
 
         # Configure speech service
-        
+        # Configure speech service
+        speech_config = speech_sdk.SpeechConfig(cog_key, cog_region)
+        print('Ready to use speech service in:', speech_config.region)
 
         # Get spoken input
         command = TranscribeCommand()
         if command.lower() == 'what time is it?':
+        #if command.lower() == 'speek':
             TellTime()
 
     except Exception as ex:
@@ -29,10 +35,42 @@ def TranscribeCommand():
     command = ''
 
     # Configure speech recognition
+    # Configure speech recognition
+    audio_config = speech_sdk.AudioConfig(use_default_microphone=True)
+    speech_recognizer = speech_sdk.SpeechRecognizer(speech_config, audio_config)
+    print('Speak now...')
 
+
+    # Configure speech recognition
+    #audioFile = 'time.wav'
+    #playsound(audioFile)
+    #audio_config = speech_sdk.AudioConfig(filename=audioFile)
+    #speech_recognizer = speech_sdk.SpeechRecognizer(speech_config, audio_config)
 
     # Process speech input
+    # Process speech input
+    #speech = speech_recognizer.recognize_once_async().get()
+    #if speech.reason == speech_sdk.ResultReason.RecognizedSpeech:
+    #    command = speech.text
+    #    print(command)
+    #else:
+    #    print(speech.reason)
+    #    if speech.reason == speech_sdk.ResultReason.Canceled:
+    #        cancellation = speech.cancellation_details
+    #        print(cancellation.reason)
+    #        print(cancellation.error_details)
 
+    # Process speech input
+    speech = speech_recognizer.recognize_once_async().get()
+    if speech.reason == speech_sdk.ResultReason.RecognizedSpeech:
+        command = speech.text
+        print(command)
+    else:
+        print(speech.reason)
+        if speech.reason == speech_sdk.ResultReason.Canceled:
+            cancellation = speech.cancellation_details
+            print(cancellation.reason)
+            print(cancellation.error_details)
 
     # Return the command
     return command
@@ -44,10 +82,26 @@ def TellTime():
 
 
     # Configure speech synthesis
-    
+    # Configure speech synthesis
+    speech_config.speech_synthesis_voice_name = "en-GB-RyanNeural"
+    speech_synthesizer = speech_sdk.SpeechSynthesizer(speech_config)
 
     # Synthesize spoken output
-
+    # Synthesize spoken output
+    speak = speech_synthesizer.speak_text_async(response_text).get()
+    #speak = speech_synthesizer.speak_text_async("Selma, what are you cooking ?").get()
+     # Synthesize spoken output
+    responseSsml = " \
+        <speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xml:lang='en-US'> \
+            <voice name='en-GB-LibbyNeural'> \
+                {} \
+                <break strength='weak'/> \
+                Time to end this lab! \
+            </voice> \
+        </speak>".format(response_text)
+    speak = speech_synthesizer.speak_ssml_async(responseSsml).get()
+    if speak.reason != speech_sdk.ResultReason.SynthesizingAudioCompleted:
+        print(speak.reason)
 
     # Print the response
     print(response_text)
